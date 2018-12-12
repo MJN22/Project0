bff9259f84d2f13ed2770a51460a071ee254282e
Marty McFly <=>
2018-12-12 09:54:14 -0600

    added pizzarepository

--- Papp.DataAccess/PizzaRepository.cs
+++ Papp.DataAccess/PizzaRepository.cs
@@ -1,8 +1,25 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
-public class Class1
+namespace Papp.DataAccess
 {
-	public Class1()
+	public class PizzaRepository : IPizzaRepository
 	{
-	}
+
+        public PizzaDB2Context Db { get => db; set => db = value; }
+
+        private PizzaDB2Context db;
+
+        public IEnumerable<PizzaRepository> getId(int Id)
+        {
+            using (var db = new PizzaDB2Context())
+            {
+                return db.PizzaRepository.Where(o => o.Id == Id);
+            }
+        }
+
+    }
 }
